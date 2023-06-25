79b1cf380c8401444a31b9ae7198b1bbd56bc331
samyosm <samy.osmium@gmail.com>
2023-06-24 20:07:46 -0400

    fish ls with exa

---
+++ shell/.config/fish/functions/l.fish
@@ -0,0 +1,4 @@
+function l --wraps='exa --icons --git-ignore --color-scale -Ta' --description 'alias l exa --icons --git-ignore --color-scale -Ta'
+  exa --icons --git-ignore --color-scale -Ta $argv
+        
+end
---
+++ shell/.config/fish/functions/ls.fish
@@ -0,0 +1,4 @@
+function ls --wraps='exa --icons --git-ignore --color-scale' --description 'alias ls exa --icons --git-ignore --color-scale'
+  exa --icons --git-ignore --color-scale $argv
+        
+end
