07ec105d327d7fc718679be52970352a8cb81da7
samyosm <samy.osmium@gmail.com>
2023-06-27 02:28:37 -0400

    asdasd

--- shell/.config/fish/functions/l.fish
+++ shell/.config/fish/functions/l.fish
@@ -1,4 +1,4 @@
-function l --wraps='exa --icons --git-ignore --color-scale -Ta' --description 'alias l exa --icons --git-ignore --color-scale -Ta'
-  exa --icons --git-ignore --color-scale -Ta $argv
+function l --wraps='exa --icons --git-ignore --color-scale -Ta' --wraps='exa --icons --git-ignore --color-scale -Ta --ignore-glob .git' --wraps='exa --abghHliS --git' --wraps='exa -abghHliS --git' --description 'alias l exa -abghHliS --git'
+  exa -abghHliS --git $argv
         
 end
--- shell/.config/fish/functions/ls.fish
+++ shell/.config/fish/functions/ls.fish
@@ -1,4 +1,4 @@
-function ls --wraps='exa --icons --git-ignore --color-scale' --description 'alias ls exa --icons --git-ignore --color-scale'
+function ls --wraps='exa --icons --git-ignore --color-scale' --wraps='exa --icons --git-ignore --color-scale --ignore-glob .git' --wraps='exa --icons --git-ignore --ignore-glob .git --color-scale' --description 'alias ls exa --icons --git-ignore --color-scale'
   exa --icons --git-ignore --color-scale $argv
         
 end
